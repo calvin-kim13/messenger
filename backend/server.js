8246393e49e44a9bb24a1cd3e3495e9bf10abf1d
Calvin Kim <calvinkim1230@gmail.com>
2022-10-31 19:17:42 -0700

    initialized concurrently to work on both frontend and backend and created a template home route on server file

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
