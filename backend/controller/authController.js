45e4e9c318407ef57adae38b8f0ada75f03dfaf6
Calvin Kim <calvinkim1230@gmail.com>
2022-11-02 17:28:40 -0700

    adding validation to registration form

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,9 +1,31 @@
 const formidable = require("formidable");
+const validator = require("validator");
 
 module.exports.userRegister = (req, res) => {
   const form = formidable();
   form.parse(req, async (err, fields, files) => {
-    console.log(fields);
+    const { userName, email, password, confirmPassword } = fields;
+    const { image } = files;
+    const error = [];
+
+    if (!userName) {
+      error.push("Please provide your user name");
+    }
+    if (!email) {
+      error.push("Please provide your email");
+    }
+    if (email && !validator.isEmail(email)) {
+      error.push("Please provide a valid email");
+    }
+    if (!password) {
+      error.push("Please provide your password");
+    }
+    if (!confirmPassword) {
+      error.push("Please confirm your password");
+    }
+    if (password && confirmPassword && password !== confirmPassword) {
+      error.push("Passwords do not match");
+    }
   });
 
   console.log("register working");
