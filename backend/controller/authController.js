b1de6a1b8bd9e6130a10e8bc656d1feaf24814f8
Calvin Kim <calvinkim1230@gmail.com>
2023-01-11 17:21:48 -0800

    completed backend for registration and connected to mongo from registration form

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,9 @@
 const formidable = require("formidable");
 const validator = require("validator");
+const registerModel = require("../models/authModel");
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports.userRegister = (req, res) => {
   const form = formidable();
@@ -39,8 +43,71 @@ module.exports.userRegister = (req, res) => {
         },
       });
     } else {
-      const getImageName = files.image.originalFilename;
-      console.log(getImageName);
+      const getImageName = image.originalFilename;
+      const randNumber = Math.floor(Math.random() * 99999);
+      const newImageName = randNumber + getImageName;
+      image.originalFilename = newImageName;
+
+      const newPath = `/Users/calvinkim/Desktop/projects/messenger/frontend/public/image/${image.originalFilename}`;
+
+      try {
+        const checkUser = await registerModel.findOne({
+          email: email,
+        });
+        if (checkUser) {
+          res.status(404).json({
+            error: {
+              errorMessage: ["Your email already exists"],
+            },
+          });
+        } else {
+          fs.copyFile(image.filepath, newPath, async (error) => {
+            if (!error) {
+              const userCreate = await registerModel.create({
+                userName,
+                email,
+                password: await bcrypt.hash(password, 10),
+                image: image.originalFilename,
+              });
+              const token = jwt.sign(
+                {
+                  id: userCreate._id,
+                  email: userCreate.email,
+                  userName: userCreate.userName,
+                  image: userCreate.image,
+                  registerTime: userCreate.createdAt,
+                },
+                process.env.SECRET,
+                {
+                  expiresIn: process.env.TOKEN_EXP,
+                }
+              );
+              const options = {
+                expires: new Date(
+                  Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000
+                ),
+              };
+
+              res.status(201).cookie("authToken", token, options).json({
+                successMessage: "Your register was successful",
+                token,
+              });
+            } else {
+              res.status(500).json({
+                error: {
+                  errorMessage: ["Internal Server Error"],
+                },
+              });
+            }
+          });
+        }
+      } catch (error) {
+        res.status(500).json({
+          error: {
+            errorMessage: ["Internal Server Error"],
+          },
+        });
+      }
     }
   });
 };
